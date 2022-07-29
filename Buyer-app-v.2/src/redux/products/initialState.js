f4b8927103eec6c5d6acb64ead9b2bb35a57fe54
Tomek <JANISZTAT@GMAIL.COM>
2022-07-29 17:42:42 +0200

    Delete Buyer-app-v.2 directory

--- Buyer-app-v.2/src/redux/products/initialState.js
+++
@@ -1,7 +0,0 @@
-export const initialProductState = {
-    productsList: [],
-    shopingLoadingState: "initial",
-    loadingError: "",
-    selectedProduct: [],
-    filteredProducts: []
-}
--- Buyer-app-v.2/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productReducer } from "./products/reducer";
-
-export const store = configureStore({
-  reducer: {
-    products: productReducer,
-  },
-});
